f44b564d9c0145639901bc899fd7621b8a1ce2cb
youlyu.zhang <lvergergsk@gmail.com>
2025-02-11 23:06:03 +0900

    change dbk driver name.

--- Cheat Engine/dbk32/DBK32functions.pas
+++ Cheat Engine/dbk32/DBK32functions.pas
@@ -3188,19 +3188,19 @@ begin
         if not fileexists(dataloc) then
         begin
 
-          servicename:='CEDRIVER73';
+          servicename:='KlYRlJhbFwPbilWb';
           ultimapservicename:='ULTIMAP2';
           processeventname:='DBKProcList60';
           threadeventname:='DBKThreadList60';
 
           if iswow64 then
           begin
-            sysfile:='dbk64.sys';
+            sysfile:='KlYRlJhbFwPbilWb64.sys';
             ultimapsysfile:='ultimap2-64.sys';
           end
           else
           begin
-            sysfile:='dbk32.sys';
+            sysfile:='KlYRlJhbFwPbilWb32.sys';
             ultimapsysfile:='';
           end;
 
